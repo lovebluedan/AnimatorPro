88039c6aaae17a55db72c372eee2d4bdafe51651
Mr.dam <326208323@qq.com>
2019-12-23 17:59:53 +0800

    增加自定义Transition 颜色变化

--- app/src/main/java/com/mzs/myapplication/translate/SampleTranslateActivity.kt
+++ app/src/main/java/com/mzs/myapplication/translate/SampleTranslateActivity.kt
@@ -2,6 +2,7 @@ package com.mzs.myapplication.translate
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.*
@@ -11,11 +12,13 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_sample_translate.*
 import com.mzs.myapplication.R
 import android.graphics.Rect
+import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.transition.TransitionSet.ORDERING_TOGETHER
 import android.transition.Visibility.MODE_IN
 import android.view.Gravity
 import android.view.animation.AccelerateDecelerateInterpolator
+import com.mzs.myapplication.custom.ChangeColorTransition
 
 
 class SampleTranslateActivity : AppCompatActivity(), View.OnClickListener {
@@ -23,7 +26,8 @@ class SampleTranslateActivity : AppCompatActivity(), View.OnClickListener {
     companion object {
         val TAG = SampleTranslateActivity::class.java.name
     }
-    lateinit var view:View
+
+    lateinit var view: View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sample_translate)
@@ -32,6 +36,7 @@ class SampleTranslateActivity : AppCompatActivity(), View.OnClickListener {
         btn_change_clip_bounds.setOnClickListener(this)
         btn_change_scroll.setOnClickListener(this)
         button.setOnClickListener(this)
+        button3.setOnClickListener(this)
     }
 
     var i: Int = 0
@@ -52,7 +57,7 @@ class SampleTranslateActivity : AppCompatActivity(), View.OnClickListener {
                         Log.i(TAG, "Fade+Slide 淡出淡入效果跟幻灯片效果")
                     }
                     1 -> {
-                        val translation =  Slide().apply {
+                        val translation = Slide().apply {
                             duration = 2000
                             interpolator = AccelerateDecelerateInterpolator()
                             slideEdge = Gravity.TOP
@@ -70,11 +75,10 @@ class SampleTranslateActivity : AppCompatActivity(), View.OnClickListener {
 //                        Log.i(TAG, "AutoTransition-- 其实里面实现的也是淡出淡入效果")
 //                    }
                 }
-                toggleVisibility(view_text,view_blue, view1_red, view_yellow)
+                toggleVisibility(view_text, view_blue, view1_red, view_yellow)
             }
             //点击按钮
             R.id.btn_change_bounds -> {
-            TransitionInflater.from(this).inflateTransition(R.transition.fade_transtion)
                 TransitionManager.beginDelayedTransition(root_view, ChangeBounds())
                 var lp = view1_red.layoutParams
                 if (lp.height == 500) {
@@ -100,15 +104,26 @@ class SampleTranslateActivity : AppCompatActivity(), View.OnClickListener {
                     val t = ChangeScroll()
                     TransitionManager.beginDelayedTransition(root_view, t)
                 }
-                if(view_text.scrollX == -50 && view_text.scrollY == -50){
-                    view_text.scrollTo(0,0)
-                }else{
-                    view_text.scrollTo(-50,-50)
+                if (view_text.scrollX == -50 && view_text.scrollY == -50) {
+                    view_text.scrollTo(0, 0)
+                } else {
+                    view_text.scrollTo(-50, -50)
                 }
             }
-            R.id.button->{
-             val intent = Intent(this,TransitionActivity1::class.java)
-             startActivity(intent)
+            R.id.button -> {
+                val intent = Intent(this, TransitionActivity1::class.java)
+                startActivity(intent)
+            }
+            R.id.button3 -> {
+                var changeColorTransition = ChangeColorTransition()
+                changeColorTransition.duration = 2000
+                TransitionManager.beginDelayedTransition(root_view, changeColorTransition)
+                val backDrawable = view1_red.background as ColorDrawable
+                if (backDrawable.color == Color.RED) {
+                    view1_red.setBackgroundColor(Color.BLUE)
+                } else {
+                    view1_red.setBackgroundColor(Color.RED)
+                }
             }
         }
     }
